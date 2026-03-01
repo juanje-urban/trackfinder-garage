0b22e18b9b123f782ee78b3cdcedcb0c988c5507
JuanjeUrbán <jurbang@uoc.edu>
2026-03-01 19:02:11 +0100

    Fix conexión a BBDD con H2 para el pipeline

--- backend/src/test/java/com/trackfindergarage/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/trackfindergarage/backend/BackendApplicationTests.java
@@ -2,6 +2,9 @@ package com.trackfindergarage.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
 
 @SpringBootTest
 class BackendApplicationTests {
