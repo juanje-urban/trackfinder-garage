379687bed5f302e7f7af7fbb56f8ca21b73c8e2c
JuanjeUrbán <jurbang@uoc.edu>
2026-04-06 09:19:05 +0200

    Fix API de récords de vuelta 2.

--- backend/src/main/java/com/trackfindergarage/backend/infrastructure/adapter/out/persistence/SpringDataLapTimeRepository.java
+++ backend/src/main/java/com/trackfindergarage/backend/infrastructure/adapter/out/persistence/SpringDataLapTimeRepository.java
@@ -1,15 +1,28 @@
 package com.trackfindergarage.backend.infrastructure.adapter.out.persistence;
 
 import com.trackfindergarage.backend.domain.model.LapTime;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SpringDataLapTimeRepository extends JpaRepository<LapTime, Long> {
 
+    @Override
+    @EntityGraph(attributePaths = {"user", "track"})
+    Optional<LapTime> findById(Long id);
+
+    @Override
+    @EntityGraph(attributePaths = {"user", "track"})
+    List<LapTime> findAll();
+
+    @EntityGraph(attributePaths = {"user", "track"})
     List<LapTime> findByUserId(Long userId);
 
+    @EntityGraph(attributePaths = {"user", "track"})
     List<LapTime> findByTrackId(Long trackId);
 
+    @EntityGraph(attributePaths = {"user", "track"})
     List<LapTime> findByUserIdAndTrackId(Long userId, Long trackId);
 }
